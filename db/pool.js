77acc81e97dc42235e0841ef8b71efe434ad3ea7
NandishBadami <nandishbadamigoogly@gmail.com>
2026-01-21 20:46:02 +0530

    hidded the database credentials

--- db/pool.js
+++ db/pool.js
@@ -1,39 +1,15 @@
 const { Pool } = require('pg');
+require('dotenv').config();
 
 const config = {
-    user: "avnadmin",
-    password: "AVNS_Rz7pqO7s5PiE7g8XSqA",
-    host: "pg-35456d83-nandishbadamigoogly-f5b9.j.aivencloud.com",
-    port: 27859,
-    database: "defaultdb",
+    user: process.env.user,
+    password: process.env.password,
+    host: process.env.host,
+    port: process.env.port,
+    database: process.env.database,
     ssl: {
         rejectUnauthorized: true,
-        ca: `-----BEGIN CERTIFICATE-----
-MIIEUDCCArigAwIBAgIUN0BIiNIc0aH7jIOcrmsfrokPL9EwDQYJKoZIhvcNAQEM
-BQAwQDE+MDwGA1UEAww1ZTc0ODczMzEtZjIxNi00M2MyLTlhNmQtNTg1M2VlMDAy
-MTI2IEdFTiAxIFByb2plY3QgQ0EwHhcNMjYwMTE5MDYzODUxWhcNMzYwMTE3MDYz
-ODUxWjBAMT4wPAYDVQQDDDVlNzQ4NzMzMS1mMjE2LTQzYzItOWE2ZC01ODUzZWUw
-MDIxMjYgR0VOIDEgUHJvamVjdCBDQTCCAaIwDQYJKoZIhvcNAQEBBQADggGPADCC
-AYoCggGBALRHxMG5CF9JblR7IYoS5vu1OV8Kx7fMQUGjiOtL11jmdMFtpU3xbu2x
-5kJWJQFzSmB+7k+BOE2mDzQo6j5HL8carrDVJgbDiRUaE9iWIV8I/8n9xf7TXCcH
-DQv89Vuv+CnrnqpYQV4Lc2dizDvcCGyQiBGBh8So5aQWCkNhnb/4IN+qQUQckkyU
-VE6TNjXJ5Yq2ENdivpNtOpjUi051dZq7dDh0Vs7WH35YyMcK8cncmDoUCBZg6ODd
-4nNSRwTvZGtfpQtsf3JhXOJAc4Q/yZioRP1f68QYUFcbUjlSJsl/G4CgBL7wd6J6
-8R9g7GYN3w0DpShWc241gN53a/mdfW55VicG2STW695smtfU11skzZK7d5cYekNQ
-Q/DizJ3CyBvaMi8+5+1+0sJg0WB0B/2030nPBWNlC+nEmPlQKDVMaTA9Dx1fmJvN
-j2E5Usti862u7iSttp1I41zevECBs7568gqVY4VV/1cWUDPXmnYYNnvEG7v3Txiv
-RUZOVWA5cQIDAQABo0IwQDAdBgNVHQ4EFgQUgxeOsZi5QmSGHfpZYgYDcMsdOY4w
-EgYDVR0TAQH/BAgwBgEB/wIBADALBgNVHQ8EBAMCAQYwDQYJKoZIhvcNAQEMBQAD
-ggGBAAUC6ZO3r+SOKrNfayAYyFDBkc62Q6Ccr5EaShS0RPAHES96g0lJB1bBF9sH
-4a43DR6hrxne3MUdv2iaJUJ2EwAE7ZQ7X9FMgSgqHE3rppqIP6TOq58vI4+hEQp0
-sMjJmgl1xqnPC87baCGxVtg9ewh76BjZ2NkCyqYjBgEWFVFQSUHAPRPxA9nsWjFZ
-lSMk4qhSxXs4JEgYK/pcfdNt4gSzMtvkhMD9wNiYIzG3xJMjXu3JA9pkZgIu4d6q
-bAuBfwQBm5Z8ObdKkk3AJsH08cN7C73w9ovmXLaPpLOsjUwpJESehqWLVp6kq68n
-IwLTGMlf4M7dw16dTPsowWa3JRedZwAPLlW7Wzu3l4d1KwKHJvB/fmBaE/GlPwAT
-R9xrPoFQHDHsUiVG6DTFZBneWWCjaJ/oj2menpYiY04W1MJJTn/WBtv/5UA+KFIO
-LiG0aX2mfxyWkF9BbhBfb+MpXc4bPiWYDzrrQSBoSk2zV7AJLLx6Yc9zyv8Ys+IN
-MoXzYA==
------END CERTIFICATE-----`,
+        ca: process.env.ca,
     },
 };
 
